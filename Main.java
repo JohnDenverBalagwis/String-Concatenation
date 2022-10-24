d509f1354aba25dba325bf3fa5692b4edbab0394
JD <91642296+JohnDenverBalagwis@users.noreply.github.com>
2022-10-24 14:22:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    //tell me about yourself using string concatenation and variables
+    String name = "John";
+    int age = 24;
+    String hometown = "Santa Cruz, Marinduque";
+    System.out.println(String.format("My name is %s. I am %d years old. I am from %s.", name, age, hometown));
+    System.out.println("I am currently taking BSIT at Marinduque State College.");
+  }
+}
